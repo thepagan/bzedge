b34be9d7b80346cce9fcc2bc44e7559529309a0a
miodragpop <miodragpopovic@stcable.net>
2020-11-22 11:40:15 +0100

    updated checksum for bzedge-vendored-crates

--- depends/packages/bzedge_vendored_crates.mk
+++ depends/packages/bzedge_vendored_crates.mk
@@ -3,6 +3,6 @@ $(package)_version=v1.0
 $(package)_download_path=https://github.com/miodragpop/bzedge-vendored-crates/releases/download/$($(package)_version)
 $(package)_file_name=bzedge-vendored-crates.tar.gz
 $(package)_download_file=bzedge-vendored-crates.tar.gz
-$(package)_sha256_hash=a8cbeddba131ef28f07b6799ae60bf89ac1999992e7e01a00a2e8529b155f44a
+$(package)_sha256_hash=104d8fe5a90d6044a9117a4c34b5185ca9ed5c732ada842f334112da6c214999
 
 
