1730491dc3057234790bc940d71556789d139b6f
miodragpop <miodragpopovic@stcable.net>
2021-04-26 23:14:03 +0200

    updated vendored crates

--- depends/packages/bzedge_vendored_crates.mk
+++ depends/packages/bzedge_vendored_crates.mk
@@ -1,6 +1,6 @@
 package=bzedge_vendored_crates
-$(package)_version=v2.0
+$(package)_version=v3.0
 $(package)_download_path=https://github.com/bze-alphateam/bzedge-vendored-crates/releases/download/$($(package)_version)
 $(package)_file_name=bzedge-vendored-crates.tar.gz
 $(package)_download_file=bzedge-vendored-crates.tar.gz
-$(package)_sha256_hash=e80c6cc73142751791affaac5c97e5d9fd197bcdbee4078d48eb59807b14112a
+$(package)_sha256_hash=568470898ce1c24497d5ff7a963ec134d63171331ec54e5cfdc44fbf26b32efa
